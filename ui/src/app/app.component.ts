7939632e536be81798b768909ba663daab1b4f20
pochii-cloud <johnmwanikig2000@gmail.com>
2023-12-12 18:02:06 +0300

    added sidenav

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { SidenavComponent } from "./components/sidenav/sidenav.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet,ButtonModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [CommonModule, RouterOutlet, ButtonModule, SidenavComponent]
 })
 export class AppComponent {
   title = 'Health-Hub';
