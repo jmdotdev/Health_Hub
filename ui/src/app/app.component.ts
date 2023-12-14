87f54c4cd1c3c6135639f133a86806c9495218c3
pochii-cloud <johnmwanikig2000@gmail.com>
2023-12-14 21:15:51 +0300

    added dashboard component

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { SidenavComponent } from "./components/sidenav/sidenav.component";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet, ButtonModule, SidenavComponent]
+    imports: [CommonModule, RouterOutlet, ButtonModule, SidenavComponent, DashboardComponent]
 })
 export class AppComponent {
   title = 'Health-Hub';
