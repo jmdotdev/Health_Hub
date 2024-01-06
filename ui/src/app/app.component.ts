d33041959c4c0f851d2f9e641826418e8b0fe105
pochii-cloud <johnmwanikig2000@gmail.com>
2024-01-06 15:45:53 +0300

    added appointments section

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -4,13 +4,14 @@ import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { SidenavComponent } from "./components/sidenav/sidenav.component";
 import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { AppointmentsComponent } from "./components/appointments/appointments.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet, ButtonModule, SidenavComponent, DashboardComponent]
+    imports: [CommonModule, RouterOutlet, ButtonModule, SidenavComponent, DashboardComponent, AppointmentsComponent]
 })
 export class AppComponent {
   title = 'Health-Hub';
