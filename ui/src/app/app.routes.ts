6d347ecfcc01ba94fa5b876ca8bd4e568ebb746b
pochii-cloud <johnmwanikig2000@gmail.com>
2024-02-11 00:15:35 +0300

    fix:fixed routing

--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import path from 'node:path';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AppointmentsComponent } from './components/appointments/appointments.component';
+import { PatientComponent } from './components/patients/patient/patient.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+{path:"",component:DashboardComponent},
+{path:'appointments',component:AppointmentsComponent},
+{path:'patients',component:PatientComponent}
+];
--- ui/src/app/components/sidenav/sidenav.component.ts
+++ ui/src/app/components/sidenav/sidenav.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterOutlet,RouterLink,RouterLinkActive],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.scss'
 })
