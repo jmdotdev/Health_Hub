6bc32bd8cbe14574016be28b253757a401cf2fa2
pochii-cloud <johnmwanikig2000@gmail.com>
2024-04-11 21:04:20 +0300

    added api

---
+++ api/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace api.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        
+    }
+}
