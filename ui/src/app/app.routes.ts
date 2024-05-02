bbe0daa2c48a5bbeacbac059c83d5c3b578b1161
pochii-cloud <johnmwanikig2000@gmail.com>
2024-05-02 23:41:38 +0300

    Fix: Fixed Patient Routing

--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -2,11 +2,11 @@ import { Routes } from '@angular/router';
 import path from 'node:path';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { AppointmentsComponent } from './components/appointments/appointments.component';
-import { PatientComponent } from './components/patients/patient/patient.component';
+import { PatientsComponent } from './components/patients/patients.component';
 
 
 export const routes: Routes = [
 {path:"",component:DashboardComponent},
 {path:'appointments',component:AppointmentsComponent},
-{path:'patients',component:PatientComponent}
+{path:'patients',component:PatientsComponent}
 ];
