39ea39199aca72870abe688fd94e658a5f933e95
pochii-cloud <johnmwanikig2000@gmail.com>
2024-07-04 23:43:03 +0300

    add login/register routes

--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -3,10 +3,14 @@ import path from 'node:path';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { AppointmentsComponent } from './components/appointments/appointments.component';
 import { PatientsComponent } from './components/patients/patients.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 
 export const routes: Routes = [
 {path:"",component:DashboardComponent},
 {path:'appointments',component:AppointmentsComponent},
-{path:'patients',component:PatientsComponent}
+{path:'patients',component:PatientsComponent},
+{path:'login',component:LoginComponent},
+{path:'register',component:RegisterComponent}
 ];
