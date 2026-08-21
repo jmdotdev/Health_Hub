437bee33e3ada5c163cf428e095223a03477365f
jmdotdev <johnmwanikig2000@gmail.com>
2026-08-21 12:37:27 +0300

    refactor: update outputPath structure in angular.json and remove unused provider in app.config.ts

--- ui/src/app/app.config.ts
+++ ui/src/app/app.config.ts
@@ -1,10 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
-import { provideClientHydration } from '@angular/platform-browser';
 
 import { routes } from './app.routes';
 import { provideZard } from '@/shared/core/provider/providezard';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withComponentInputBinding()), provideClientHydration(), provideZard()],
+  providers: [provideRouter(routes, withComponentInputBinding()), provideZard()],
 };
